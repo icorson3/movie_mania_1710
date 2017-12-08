3f54e8532953152da87611b7f2bd0b1908cedae5
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-08 11:00:18 -0700

    Add relationships to director spec

--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 describe Director, type: :model do
-  it {should validate_presence_of(:name)}
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+  end
+
+  describe "relationships" do
+    it {should have_many(:movies)}
+  end
 end
