ca2c7f0aad245f3cbbe78a04f5a4acf80b2040a5
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-11 10:07:44 -0700

    Add new and create for director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,4 +4,20 @@ class DirectorsController < ApplicationController
     @director = Director.new
   end
 
+  def create
+
+    director = Director.new(director_params)
+    if director.save
+      redirect_to "/directors/#{director.id}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def director_params
+      params.require(:director).permit(:name)
+    end
+
 end
