b6d4d0bee46f80512a0f03b0c0959030d4aa510b
Ilana Corson <ilana@turing.io>
2017-12-11 14:45:17 -0700

    update routes for nesting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,30 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @director = Director.find(params[:director_id])
+    @movie = Movie.new
+  end
+
+  def create
+    director = Director.find(params[:director_id])
+    movie = director.movies.new(movie_params)
+
+    if movie.save
+      redirect_to "/movies/#{movie.id}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # get '/movies', to: 'movies#index'
 
-  resources :movies
-  resources :directors
+  resources :directors, shallow: true do
+    resources :movies
+  end
+
+  resources :movies, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
