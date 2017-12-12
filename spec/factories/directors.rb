9e23dd9c41f47824f924590531c82e63ad628b25
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-12 12:28:26 -0700

    Update factory

--- spec/factories/directors.rb
+++ spec/factories/directors.rb
@@ -1,10 +1,21 @@
 FactoryBot.define do
 
+  factory :director do
+    sequence(:name) {|n| "Director#{n}"}
+  end
+
   factory :admin, class: :director do
     sequence(:name) {|n| "Director#{n}"}
   end
 
-  factory :director do
-    sequence(:name) {|n| "Director#{n}"}
+  factory :director_with_movies, class: :director do
+    name "My name"
+    transient do
+      movie_count 3
+    end
+
+    after(:create) do |director, evaluator|
+      create_list(:movie, evaluator.movie_count, director: director)
+    end
   end
 end
