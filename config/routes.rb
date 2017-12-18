568a1f1b0362a016e722f9bbdcfafb75c2e2dc15
Ali Schlereth <aschlere@sbcglobal.net>
2017-12-18 10:03:34 -0700

    Add root and usernew

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # get '/movies', to: 'movies#index'
 
+  root "welcome#index"
+
   resources :directors, shallow: true do
     resources :movies
   end
 
   resources :movies, only: [:index]
+  resources :users, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
