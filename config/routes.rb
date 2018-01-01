4d8ed0831063366dc0e27269666599c8c19af396
Ilana Corson <ilana@turing.io>
2018-01-01 12:56:23 -0700

    add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  
+
   resources :directors, shallow: true do
     resources :movies
   end
 
   resources :movies, only: [:index]
   resources :users, only: [:show, :new, :create]
+
+  namespace :admin do
+    resources :categories, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/features/admin_sees_all_categories_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "user visits categories index page" do
+  context "as an admin" do
+    it "allows admin to see all categories" do
+      admin = User.create(username: "funbucket13",
+                          password: "test",
+                          role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_categories_path
+
+      expect(page).to have_content("Admin Categories")
+    end
+  end
+
+  context "as a default user" do
+    it "does not allow default user to see admin categories index" do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_categories_path
+
+      expect(page).to_not have_content("Admin Categories")
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+end
