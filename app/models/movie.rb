2dfb6a0ae4bba09dba3b5e7bf36ef89dc83eab0f
Ali Schlereth <aschlere@sbcglobal.net>
2018-01-03 12:57:29 -0700

    Use slug callback

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,12 @@
 class Movie < ApplicationRecord
+  before_save :generate_slug
 
   belongs_to :director
+
+  private
+
+    def generate_slug
+      self.slug = title.parameterize
+    end
 
 end
---
+++ spec/features/user_can_see_a_movie_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe "user sees one movie" do
+  it "has one title and description" do
+    movie = create(:movie)
+    visit movie_path(movie.slug)
+
+    expect(page).to have_content(movie.title)
+    expect(page).to have_content(movie.description)
+  end
+end
