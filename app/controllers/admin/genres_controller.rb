e6447fda7e144fc48ffdac006201f26afc30a6fd
TMad <madsen.tyler@gmail.com>
2018-01-09 09:38:11 -0700

    add new action to index and update create action

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,6 +6,13 @@ class Admin::GenresController < Admin::BaseController
 
   def create
     @genre = Genre.new(genre_params)
+    if @genre.save
+      flash.notice = "#{@genre.name} successfully created."
+      redirect_to genres_path
+    else
+      flash.notice = "Please try to create a genre again with the correct information."
+      redirect_to genres_path
+    end
   end
 
   private
