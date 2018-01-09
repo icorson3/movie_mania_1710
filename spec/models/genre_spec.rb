1d22fd953ae60e94c6df51b30d1f94a1398ffd27
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-09 10:21:24 -0700

    Add validations for genre model spec

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "cant save without a name" do
+      genre = Genre.new
+
+      expect(genre).to be_invalid
+
+    end
+
+    it "can save with a name" do
+      genre = Genre.new(name: "TYJSDLFKJSLFDJ")
+
+      expect(genre).to be_valid
+    end
+  end
 end
