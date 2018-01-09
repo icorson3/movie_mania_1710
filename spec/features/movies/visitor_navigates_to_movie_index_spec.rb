9f000d31a0c8634ed77e248ac521e955476d1afd
seoulstice <jungyoungjoon@gmail.com>
2018-01-09 10:36:19 -0700

    story 3 passing

--- spec/features/movies/visitor_navigates_to_movie_index_spec.rb
+++ spec/features/movies/visitor_navigates_to_movie_index_spec.rb
@@ -5,7 +5,7 @@ describe "When an visitor visits movies index" do
     genre1 = Genre.create(name: "Sci-Fi")
     genre2 = Genre.create(name: "Adventure")
     genre3 = Genre.create(name: "Action")
-    @movie = create(:movie, name: "Guardians of the Galaxy")
+    @movie = create(:movie, title: "Guardians of the Galaxy", rating: 5)
     mg1 = MovieGenre.create(movie: @movie, genre: genre1)
     mg2 = MovieGenre.create(movie: @movie, genre: genre2)
     mg3 = MovieGenre.create(movie: @movie, genre: genre3)
@@ -17,24 +17,12 @@ describe "When an visitor visits movies index" do
 
     click_link "Guardians of the Galaxy"
 
-    expect(current_path).to eq(movie_path(@movie))
+    expect(current_path).to eq(movie_path(@movie.slug))
     expect(page).to have_content("Genres for this Movie:")
     expect(page).to have_content("Action")
     expect(page).to have_content("Sci-Fi")
     expect(page).to have_content("Adventure")
-    
-
-
+    expect(page).to have_content("Rating: 5")
 
   end
-
-
 end
-
-
-As an unregistered user,
-When I visit the movie index,
-And I click on "Guardians of the Galaxy"
-I see a page with "Guardians of the Galaxy",
-I also see "Action", "Adventure", "Sci-Fi" under a "Genres for this Movie:" heading
-I also see the rating for this movie (rating should be an attribute of a movie).
