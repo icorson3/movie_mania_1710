99d09b68236d8c088fb007df72f44eac65de76a3
nico24687 <theonlynico@hotmail.com>
2018-01-09 09:49:18 -0700

    second test passing

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     resources :genres 
   end
 
+  resources :genres, only: [:index]
+
   resources :carts, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/user_visits_genre_index_spec.rb
+++ spec/features/user_visits_genre_index_spec.rb
@@ -1,24 +1,45 @@
 require 'rails_helper'
 
 
-feature "admin vitis genre index page" do 
+feature "user vitis genre index page" do 
   before  do 
+   
+  end
+  scenario "as an admin they see a form to create  a genre, after completing it they see the genre " do 
+
      admin = User.create(username: "funbucket13",
                           password: "test",
                           role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
     visit admin_genres_path
-  end
-  scenario "sees a form to create a new genre " do 
+
     expect(page).to have_content("Create a genre")
     fill_in "genre[name]", with: "Comedy"
     click_on "Create Genre"
 
     expect(page).to have_content("Comedy")
   end
+
+  scenario "as an unregistered user they do not see the form to create a new genre " do 
+    visit genres_path
+    genre1 = Genre.create(name: "Comedy")
+    genre2 = Genre.create(name: "Thriller")
+
+    expect(page).to_not have_content("Create a genre")
+
+  end
+
+
 end
 
+
+# As an unregistered user,
+# When I visit the genre index,
+# I cannot see the form to create new genres, 
+# I see all genres (for testing purposes, I should see at least 2) 
+# and each genre should be a link to that genre's specific page
 
 
 
