22052864ea6155e4ce362ab65a31556b0a6c3f32
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-09 16:02:38 -0700

    model tests passing

---
+++ spec/models/genre_movie_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+describe GenreMovie, type: :model do
+  describe "relationships" do
+    it {should belong_to(:movie)}
+    it {should belong_to(:genre)}
+  end
+end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -7,20 +7,8 @@ describe Movie, type: :model do
 
   describe "relationships" do
     it {should have_many(:genres)}
+    it {should belong_to(:director)}
     it {should have_many(:genre_movies)}
   end
-
-  # describe "#average_rating" do
-  #   it "gets the average rating " do
-  #     director = create(:director)
-  #     @gog = director.movies.create(title:"Guardians of the Galaxy", rating: 5)
-  #     @ss = director.movies.create(title:"Suicide Squad", rating: 15)
-  #     @action = Genre.create(name:"action")
-  #     GenreMovie.create!(genre: @action, movie:@gog)
-  #     GenreMovie.create!(genre: @action, movie:@ss)
-  #
-  #     expect(@action.average_rating).to eq(10)
-  #   end
-  # end
 
 end
