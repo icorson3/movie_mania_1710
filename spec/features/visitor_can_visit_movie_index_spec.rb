09a9164825e885a92fe461e58124996e47414686
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-09 10:19:19 -0700

    Finished user story 3

--- spec/features/visitor_can_visit_movie_index_spec.rb
+++ spec/features/visitor_can_visit_movie_index_spec.rb
@@ -7,29 +7,31 @@ describe "As an unregistered visitor" do
 
       @movie = Movie.create(title: "Guardians of the Galaxy", description: "Great Movie!", director: @director, rating: 10)
 
+
       @genre_1 = Genre.create(name: "Action")
       @genre_2 = Genre.create(name: "Adventure")
       @genre_3 = Genre.create(name: "Sci-Fi")
 
-      @genre_movie_1 = GenreMovie.create(movie_id: @movie, genre_id: @genre_1)
-      @genre_movie_2 = GenreMovie.create(movie_id: @movie, genre_id: @genre_2)
-      @genre_movie_3 = GenreMovie.create(movie_id: @movie, genre_id: @genre_3)
+      @genre_movie_1 = GenreMovie.create(movie_id: @movie.id, genre_id: @genre_1.id)
+      @genre_movie_2 = GenreMovie.create(movie_id: @movie.id, genre_id: @genre_2.id)
+      @genre_movie_3 = GenreMovie.create(movie_id: @movie.id, genre_id: @genre_3.id)
 
-      visit movies_path
     end
     it "displays a movie title that is a hyperlink that goes to the show page for that specific movie with a rating for that movie and several generes for taht movie under a specific heading" do
+      visit movies_path
+
       expect(page).to have_content(@movie.title)
 
       click_link("#{@movie.title}")
 
-      expect(current_path).to eq(movie_path(@movie))
+      expect(current_path).to eq(movie_path(@movie.slug))
 
       expect(page).to have_content("#{@movie.title}")
 
       within("#genre-heading") do
         expect(page).to have_content("Genres for this Movie:")
       end
-
+    
       expect(page).to have_content("#{@genre_1.name}")
       expect(page).to have_content("#{@genre_2.name}")
       expect(page).to have_content("#{@genre_3.name}")
