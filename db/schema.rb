da08435793ffba573db9d81cae3d06e5194daa92
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-09 09:34:01 -0700

    building out creating new genres and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103183756) do
+ActiveRecord::Schema.define(version: 20180109162329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20180103183756) do
   end
 
   create_table "directors", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
