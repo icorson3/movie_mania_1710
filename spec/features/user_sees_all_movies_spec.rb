979d5d45ef9da724101800507bf2849d4b05ac61
TMad <madsen.tyler@gmail.com>
2018-01-09 10:11:41 -0700

    update movie index feature test

--- spec/features/user_sees_all_movies_spec.rb
+++ spec/features/user_sees_all_movies_spec.rb
@@ -9,9 +9,9 @@ describe "user_index" do
     visit "/movies"
 
     expect(page).to have_content("All Movies")
-    expect(page).to have_content(movie_1.title)
+    expect(page).to have_link(movie_1.title)
     expect(page).to have_content(movie_1.description)
-    expect(page).to have_content(movie_2.title)
+    expect(page).to have_link(movie_2.title)
     expect(page).to have_content(movie_2.description)
   end
 end
