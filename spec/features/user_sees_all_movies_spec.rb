922fa4f7a852daacfc1cb29aec4ed2c37b61a4c0
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-09 09:34:41 -0700

    finished writing tests

--- spec/features/user_sees_all_movies_spec.rb
+++ spec/features/user_sees_all_movies_spec.rb
@@ -14,4 +14,23 @@ describe "user_index" do
     expect(page).to have_content(movie_2.title)
     expect(page).to have_content(movie_2.description)
   end
+
+  xit "should show all genres belonging to that movie" do
+    user = user(:create)
+    movie_1 = create(:movie, director_id: director.id)
+    genre = Genre.create(name: "Action")
+    genre2 = Genre.create(name: "Adventure")
+    genre3 = Genre.create(name: "Sci-Fi")
+    mg = MovieGenre.create(movie: movie_1, genre: genre)
+    mg = MovieGenre.create(movie: movie_1, genre: genre2)
+    mg = MovieGenre.create(movie: movie_1, genre: genre3)
+    visit movies_path
+    click_on "#{movie_1.name}"
+
+    expect(current_path).to eq(movie_path(movie_1))
+    expect(page).to have_content("Action")
+    expect(page).to have_content("Adventure")
+    expect(page).to have_content("Sci-Fi")
+    expect(page).to have_content("Rating: 4")
+  end
 end
