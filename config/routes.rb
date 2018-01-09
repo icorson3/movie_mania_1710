3b26bf8cabbaf8cde1901cd6d861c1b86eb6da98
Tim Tyrrell <timomitchel23@gmail.com>
2018-01-09 09:16:37 -0700

    test written for user_story_1

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :categories, only: [:index]
+    resources :genres, only: [:index]
   end
 
   resources :carts, only: [:create]
---
+++ spec/features/admin_visits_genre_index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "user visits genre index page" do
+  context "as an admin" do
+    context "fill out the form on page" do 
+      it "allows admin to see all genres" do
+      admin = User.create(username: "funbucket13",
+                          password: "test",
+                          role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_genres_path
+
+      expect(page).to have_content("Genres") 
+      expect(page).to have_button("submit")
+
+      fill_in "genre[title]", with: "Comedy"
+      click_on "submit"
+
+      genre = Genre.last
+      expect(current_path).to eq("/admin/genres") 
+      expect(page).to have_content "Comedy"
+      end
+    end
+  end
+end
