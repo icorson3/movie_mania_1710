c94d05769de8855070ac1545d710e37134582f76
ZacharyLandes <zach.g.landes@gmail.com>
2018-01-09 15:59:40 -0700

    genre spec passing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,17 @@
 class Movie < ApplicationRecord
+  validates_presence_of :title
   before_save :generate_slug
 
   belongs_to :director
   has_many  :genre_movies
   has_many  :genres, through: :genre_movies
-    has_many  :category_movies
-    has_many  :categories, through: :category_movies
+  has_many  :category_movies
+  has_many  :categories, through: :category_movies
+
   private
 
-    def generate_slug
-      self.slug = title.parameterize
-    end
+  def generate_slug
+    self.slug = title.parameterize
+  end
 
 end
