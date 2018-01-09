0d24bff23fc959fd78265620ed2edfdbc857c754
leepuppychow <leepuppychow@gmail.com>
2018-01-09 10:25:34 -0700

    User Story 4 complete

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -9,4 +9,16 @@ describe Genre do
     it {should have_many(:movies).through(:genre_movies)}
   end
 
+  describe "Instance Methods" do
+    it "can calculate average_movie_rating" do
+      @genre1 = create(:genre, name: "Action")
+      @movie_1 = create(:movie, title: "Guardians of the Galaxy", rating: 80)
+      @movie_2 = create(:movie, title: "Suicide Squad", rating: 70)
+      @movie_1.genres << @genre1
+      @movie_2.genres << @genre1
+
+      expect(@genre1.average_movie_rating).to eq 75
+    end
+  end
+
 end
