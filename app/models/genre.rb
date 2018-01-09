f390e7b9196482d792af893a982f5883528f343e
abarnes26 <abarnes26@yahoo.com>
2018-01-09 10:45:16 -0700

    completed all requirements

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,7 @@
 class Genre < ApplicationRecord
   has_many :genre_movies
   has_many :movies, through: :genre_movies
+  validates_presence_of :name
 
   def average_movie_rating
     movies.average(:rating)
