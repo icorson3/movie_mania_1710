c8116c1a1456a01dbf8b57f9fbcff1f7126116ff
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-10 12:52:52 -0700

    Final commit, avg rating instance method test is created in genre_spec.rb

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe Genre, type: :model do
       expect(genre).to be_valid
     end
   end
+
+  context "instance methods" do
+    it "gets you the average movie ratings across all movies " do
+      @director = Director.create(name: "Billy Bob")
+
+      @movie = Movie.create(title: "Guardians of the Galaxy", description: "Great Movie!", director: @director, rating: 10)
+      @movie_2 = Movie.create(title: "Suicide Squad", description: "Bad Movie!", director: @director, rating: 10)
+
+
+      @genre_1 = Genre.create(name: "Action")
+
+      @genre_movie_1 = GenreMovie.create(movie_id: @movie.id, genre_id: @genre_1.id)
+      @genre_movie_2 = GenreMovie.create(movie_id: @movie_2.id, genre_id: @genre_1.id)
+
+      expect(@genre_1.avg_rating).to eq(10)
+    end
+  end
 end
