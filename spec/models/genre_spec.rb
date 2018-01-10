96e5d7b28de6d0a099766da3e5df03ab2389b7da
nico24687 <theonlynico@hotmail.com>
2018-01-09 20:09:19 -0700

    updated to have a model test for average rating method

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Genre, type: :model do
 
     expect(genre).to respond_to(:movies)
   end
+  it "can calculate the average rating fo movies" do 
+    director = Director.create(name: "Steve")
+    movie1 = director.movies.create(title: "Guardians of the  Galaxy", description: "people get lost", rating: 10)
+    movie2 = director.movies.create(title: "Suicide Squad", description: "peopele fighting", rating: 2)
+    genre1 = Genre.create(name: "Action")
+    MovieGenre.create(movie_id: movie1.id, genre_id: genre1.id)
+    MovieGenre.create(movie_id: movie2.id, genre_id: genre1.id)
+
+    expect(genre1.average_rating). to eq(6)
+
+  end
 end
